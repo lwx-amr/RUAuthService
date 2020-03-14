d032de9dfe0a84bc25ad9ed7d32924acd8876f16
lwx-amr <amr.ister20@gmail.com>
2020-03-14 03:01:17 +0200

    GET /Login routes is now returning json

--- api/loginRoutes.js
+++ api/loginRoutes.js
@@ -4,9 +4,7 @@ import { login, checkAuthen} from "../service/loginController";
 const router = express.Router();
 
 router.get("/login", checkAuthen, (req, res) => {
-    // res.writeHead(200, {'Content-Type': 'text/plain'});
-    // res.end('Hello from the server side');
-    res.contentType('text').send("Hello, we are very happy that you're here");
+    res.json("Hello, we are very happy that you're here");
 });
 router.post("/login", login);
 
